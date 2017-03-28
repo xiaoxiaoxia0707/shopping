693af5647eaddbcf7bb779d1a02fe521c442f190
sasha <1217435098@qq.com>
2017-03-28 15:57:12 +0800

    改了活动与机构转换

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,3 +1,5 @@
+/*商城首页*/
+//轮播图
 $(".swiper-container").swiper({
     pagination: '.swiper-pagination',
     paginationClickable: true,
@@ -5,4 +7,15 @@ $(".swiper-container").swiper({
     centeredSlides: true,
     autoplay: 2000,
     autoplayDisableOnInteraction: false
-})
+});
+//新闻与热点的转换
+$(".buttons-tab a").click(function() {
+	$(".buttons-tab a").removeClass("active");
+    $(this).addClass("active");
+});
+//工具栏首页、捐款、拼图和个人中心的转换
+$(".bar-tab a").click(function() {
+	$(".bar-tab a").removeClass("active");
+    $(this).addClass("active");
+});
+ 
