286ea4133b2cb80f286270d037b95ede256e49a6
sasha <1217435098@qq.com>
2017-03-29 10:46:45 +0800

    1

--- dist/js/layout.js
+++ dist/js/layout.js
@@ -18,4 +18,9 @@ $(".bar-tab a").click(function() {
 	$(".bar-tab a").removeClass("active");
     $(this).addClass("active");
 });
- 
+/*分类页面的菜单栏*/
+$("#menu-list-group li").click(function(){
+    $("#menu-list-group li").removeClass("active");
+     $(this).addClass("active");
+    
+})
