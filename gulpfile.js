45f2e617e2be5e9a3adb75dd47a73cf6b6e2192c
sasha <1217435098@qq.com>
2017-03-29 15:17:25 +0800

    新增资料填写页面

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     less = require('gulp-less'), //less编译css文件
     // rev = require('gulp-rev'); 
+    imagemin = require('gulp-imagemin'),//压缩图片
 	clean = require('gulp-clean'), //清理
     runSequence = require('run-sequence'); //顺序执行任务
 // 样式 
@@ -17,6 +18,17 @@ gulp.task('scripts', function() {
     gulp.src(['src/js/*.js'])
         .pipe(gulp.dest('dist/js'));
 });
+// Images
+gulp.task('images', function() {
+    gulp.src('src/images/*.png')
+    .pipe(imagemin({
+            optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+        }))
+    .pipe(gulp.dest('dist/images'));
+});
 //html
 gulp.task('html', function() {
     gulp.src('src/*.html')
@@ -24,10 +36,10 @@ gulp.task('html', function() {
 });
 // 清理
 gulp.task('clean', function() {
-    return gulp.src(['dist/css', 'dist/js'], { read: false })
+    return gulp.src(['dist/css', 'dist/js','dist/images'], { read: false })
         .pipe(clean());
 });
 // 预设任务
 gulp.task('default', function(callback) {
-    runSequence('clean', 'testLess', 'scripts', 'html', callback);
+    runSequence('clean', 'testLess', 'scripts', 'images','html', callback);
 });
