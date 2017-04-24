cdd6d7f47ab36a8d97b6719030c4c00d93859b80
sasha <1217435098@qq.com>
2017-04-24 13:36:38 +0800

    写出js的商品详情规格

--- dist/js/productDetails.js
+++ dist/js/productDetails.js
@@ -1,3 +1,4 @@
+
 //商品详情轮播图
 $(".swiper-container").swiper({
     pagination: '.swiper-pagination',
@@ -7,3 +8,36 @@ $(".swiper-container").swiper({
     spaceBetween: 30,
     loop: true
 });
+$(document).on('click','#create-actions', function () {
+      var content= [
+        {
+          text: '<div><p>尺码</p><div><span>M</span><span>L</span><span>XL</span><span>2XL</span></div></div>',
+           bold: true,
+           onClick: function() {
+            $.alert("你选择了“M“");
+          }
+        },
+         {
+          text: '<div><p>主要颜色</p><div><span>黑色</span><span>酒红色</span></div></div>',
+          bold: true,
+          onClick: function() {
+            $.alert("你选择了“黑色“");
+          }
+          
+        },
+        {
+          text: '<div><p>购买数量</p><div><a> <span class="btn reduce" min="1">-</span></a><input type="number" min="1" value="1"><span class="btn plus" max="1366">+</span></div></div>',
+          bold: true,
+           onClick: function() {
+            $.alert("你选择了“1“");
+          }
+        }
+      ];
+      var buttons2 = [	
+        {
+          text: '加入购物车'
+        }
+      ];
+      var groups = [content, buttons2];
+      $.actions(groups);
+  });
